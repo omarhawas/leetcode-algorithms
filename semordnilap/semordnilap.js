f8d171e96769bd90d0bdd3cdca7e767b269349d4
Omar Hawas <omarhawas94@gmail.com>
2024-07-31 16:06:41 -0400

    small change

--- semordnilap/semordnilap.js
+++ semordnilap/semordnilap.js
@@ -2,22 +2,21 @@ function semordnilap(words) {
     const wordsSeen = {};
     const result = [];
   
-    // Track all words in wordsSeen object
+    // track all words in wordsSeen object
     for (let i = 0; i < words.length; i++) {
       wordsSeen[words[i]] = true;
     }
   
-    // Check for semordnilap pairs
+    // check for semordnilap pairs
     for (let i = 0; i < words.length; i++) {
       let word = words[i];
       let reversedWord = word.split("").reverse().join("");
       if (reversedWord in wordsSeen && reversedWord !== word) {
         result.push([word, reversedWord]);
-        // Remove the words from wordsSeen to avoid duplicates
+        // remove the words from wordsSeen 
         delete wordsSeen[word];
         delete wordsSeen[reversedWord];
       }
     }
     return result;
 }
-  
